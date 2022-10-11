1b255ed98425ce6bdb541fa687efeac6ca1d1818
Destin Liu <destin.liu@outlook.com>
2022-10-11 11:27:49 +0800

    move buildinfo

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -e
 
 CWD="$(cd $(dirname $0); pwd)"
 
-rm -rf $CWD/buildinfo
 rm -rf $CWD/EFI
 rm -rf $CWD/downloads
 
@@ -18,7 +17,7 @@ mkdir -pv $CWD/downloads
 r=acidanthera/OpenCorePkg
 t=$CWD/downloads/OpenCorePkg
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
-echo "OpenCore version: $v" | tee -a buildinfo
+echo "OpenCore version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/OpenCore.zip https://github.com/$r/releases/download/$v/OpenCore-$v-RELEASE.zip
 unzip -q $CWD/downloads/OpenCore.zip -d $t
 cp -v $t/X64/EFI/BOOT/BOOTx64.efi           $CWD/EFI/BOOT/
@@ -35,7 +34,7 @@ cp -v $t/Drivers/HfsPlus.efi $CWD/EFI/OC/Drivers/
 r=acidanthera/Lilu
 t=$CWD/downloads/Lilu
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
-echo "Lilu version: $v" | tee -a buildinfo
+echo "Lilu version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/Lilu.zip https://github.com/$r/releases/download/$v/Lilu-$v-RELEASE.zip
 unzip -q $CWD/downloads/Lilu.zip -d $t
 cp -rv $t/Lilu.kext $CWD/EFI/OC/Kexts/
@@ -43,7 +42,7 @@ cp -rv $t/Lilu.kext $CWD/EFI/OC/Kexts/
 r=acidanthera/VirtualSMC
 t=$CWD/downloads/VirtualSMC
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
-echo "VirtualSMC version: $v" | tee -a buildinfo
+echo "VirtualSMC version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/VirtualSMC.zip https://github.com/$r/releases/download/$v/VirtualSMC-$v-RELEASE.zip
 unzip -q $CWD/downloads/VirtualSMC.zip -d $t
 cp -rv $t/Kexts/SMCBatteryManager.kext $CWD/EFI/OC/Kexts/
@@ -54,7 +53,7 @@ cp -rv $t/Kexts/VirtualSMC.kext        $CWD/EFI/OC/Kexts/
 r=acidanthera/WhateverGreen
 t=$CWD/downloads/WhateverGreen
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
-echo "WhateverGreen version: $v" | tee -a buildinfo
+echo "WhateverGreen version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/WhateverGreen.zip https://github.com/$r/releases/download/$v/WhateverGreen-$v-RELEASE.zip
 unzip -q $CWD/downloads/WhateverGreen.zip -d $t
 cp -rv $t/WhateverGreen.kext $CWD/EFI/OC/Kexts/
@@ -62,7 +61,7 @@ cp -rv $t/WhateverGreen.kext $CWD/EFI/OC/Kexts/
 r=acidanthera/AppleALC
 t=$CWD/downloads/AppleALC
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
-echo "AppleALC version: $v" | tee -a buildinfo
+echo "AppleALC version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/AppleALC.zip https://github.com/$r/releases/download/$v/AppleALC-$v-RELEASE.zip
 unzip -q $CWD/downloads/AppleALC.zip -d $t
 cp -rv $t/AppleALC.kext $CWD/EFI/OC/Kexts/
@@ -71,7 +70,7 @@ r=acidanthera/VoodooPS2
 t=$CWD/downloads/VoodooPS2Controller
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
 v=${v#v}
-echo "VoodooPS2 version: $v" | tee -a buildinfo
+echo "VoodooPS2 version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/VoodooPS2Controller.zip https://github.com/$r/releases/download/v$v/VoodooPS2Controller-$v-RELEASE.zip
 unzip -q $CWD/downloads/VoodooPS2Controller.zip -d $t
 cp -rv $t/VoodooPS2Controller.kext $CWD/EFI/OC/Kexts/
@@ -79,7 +78,7 @@ cp -rv $t/VoodooPS2Controller.kext $CWD/EFI/OC/Kexts/
 r=VoodooI2C/VoodooI2C
 t=$CWD/downloads/VoodooI2C
 v=$(curl -sL https://api.github.com/repos/$r/releases/latest | jq -r ".tag_name")
-echo "VoodooI2C version: $v" | tee -a buildinfo
+echo "VoodooI2C version: $v" | tee -a $CWD/EFI/buildinfo
 curl -sL -o $CWD/downloads/VoodooI2C.zip https://github.com/$r/releases/download/$v/VoodooI2C-$v.zip
 unzip -q $CWD/downloads/VoodooI2C.zip -d $t
 cp -rv $t/VoodooI2C.kext    $CWD/EFI/OC/Kexts/
